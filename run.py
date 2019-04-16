e8890ef7d2d35f62e3c1e0cacffbe3c23a771d0d
Dmitry Kryuchkov <dmitry.kryuchkov@cultureamp.com>
2019-04-16 18:10:06 +1000

    ignore timeouts

--- run.py
+++ run.py
@@ -2,23 +2,30 @@
 
 import time
 import logging
-from bluetoothctl import Bluetoothctl
+from bluetoothctl import Bluetoothctl, pexpect
 
 SLEEP_SECONDS = 5
 YAMAHA_MAC = '00:A0:DE:04:07:17'
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
+
+    log = logging.getLogger()
 
     ctl = Bluetoothctl()
     while True:
-        time.sleep(SLEEP_SECONDS)
-        if ctl.is_connected():
+        try:
+            if ctl.is_connected():
+                continue
+
+            log.info('Device is not connected, attempting to connect')
+            ctl.power_on()
+            ctl.connect(YAMAHA_MAC)
+        except pexpect.exceptions.TIMEOUT:
             continue
 
-        ctl.power_on()
-        ctl.connect(YAMAHA_MAC)
+        time.sleep(SLEEP_SECONDS)
 
 
 if __name__ == '__main__':
